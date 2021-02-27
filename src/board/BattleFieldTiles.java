d00d4f5ee00c73da754167ac2b806ec25e48683f
Martin Petrov <63491429+petrovmartin2610@users.noreply.github.com>
2021-02-27 17:03:50 +0300

    Third commit for pu-oop-project

---
+++ src/board/BattleFieldTiles.java
@@ -0,0 +1,23 @@
+package board;
+import java.awt.*;
+
+public class BattleFieldTiles {
+    private int row;
+    private int col;
+    Color color;
+    private int tileSize=10;
+
+    public BattleFieldTiles(int row, int col, Color color){
+        this.row=row;
+        this.col=col;
+        this.color=color;
+    }
+
+    public void render(Graphics g) {
+        int tileX = this.col * tileSize;
+        int tileY = this.row * tileSize;
+        g.setColor(this.color);
+        g.fillRect(tileX, tileY, tileSize, tileSize);
+    }
+
+}
--- src/board/Battlefield.java
+++ src/board/Battlefield.java
@@ -5,6 +5,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class Battlefield extends JFrame implements MouseListener {
+    private Object[][] battleField;
 
 
 
